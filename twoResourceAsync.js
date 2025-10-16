bde4ab1f3cae30a9fd36e8ef1a6c7ecd5f3603bb
Rishabh Sundli <rishabhsundli024@gmail.com>
2025-10-16 12:54:39 +0530

    Initialised the functions

--- twoResourceAsync.js
+++ twoResourceAsync.js
@@ -1,1 +1,27 @@
-console.log("helo");
+function fetchA(){
+    let random=Math.floor(Math.random()*10);
+    return new Promise((resolve,reject)=>{
+       setTimeout(()=>{
+        if(random>5){
+        resolve(`Network access granted`);
+       }else{
+        reject(`Network access terminated`);
+       }  
+       },500)
+    }).then(n=>console.log(n)).catch(n=>console.log(n))
+}
+
+function fetchB(){
+    let random=Math.floor(Math.random()*10);
+    return new Promise((resolve,reject)=>{
+       setTimeout(()=>{
+        if(random>5){
+        resolve(`Network access granted`);
+       }else{
+        reject(`Network access terminated`);
+       }  
+       },1700)
+    }).then(n=>console.log(n)).catch(n=>console.log(n))
+}
+fetchA();
+fetchB();
