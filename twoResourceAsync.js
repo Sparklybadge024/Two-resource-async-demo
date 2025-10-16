a1385d006c54af6eea4e3a70cec3e4109e40ed04
Rishabh Sundli <rishabhsundli024@gmail.com>
2025-10-16 13:33:52 +0530

    Functions with promises created

--- twoResourceAsync.js
+++ twoResourceAsync.js
@@ -1,27 +1,29 @@
-function fetchA(){
-    let random=Math.floor(Math.random()*10);
-    return new Promise((resolve,reject)=>{
-       setTimeout(()=>{
-        if(random>5){
-        resolve(`Network access granted`);
-       }else{
-        reject(`Network access terminated`);
-       }  
-       },500)
-    }).then(n=>console.log(n)).catch(n=>console.log(n))
-}
+    function fetchA(){
+        let random=Math.floor(Math.random()*10);
+        return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            if(random>5){
+            resolve(`Network access granted`);
+        }else{
+            reject(`Network access terminated`);
+        }  
+        },Math.floor(Math.random()*1500))
+        })
+    }
 
-function fetchB(){
-    let random=Math.floor(Math.random()*10);
-    return new Promise((resolve,reject)=>{
-       setTimeout(()=>{
-        if(random>5){
-        resolve(`Network access granted`);
-       }else{
-        reject(`Network access terminated`);
-       }  
-       },1700)
-    }).then(n=>console.log(n)).catch(n=>console.log(n))
-}
-fetchA();
-fetchB();
+    function fetchB(){
+        let random=Math.floor(Math.random()*10);
+        return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            if(random>5){
+            resolve(`Network access granted`);
+        }else{
+            reject(`Network access terminated`);
+        }  
+        },Math.floor(Math.random()*1500))
+        })
+    }
+    
+    
+
+
